67d97d0af2b98917f426506877746dd8332422bd
Elliot Erickson <elliot_erickson@protonmail.com>
2023-03-10 10:21:20 -0800

    Added boilerplate code to all components

---
+++ src/Components/ContactMe.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactMe = () => {
+    return (
+        <div className='contact-me' id='contact-me'>
+            <h1>Contact Me</h1>
+        </div>
+    )
+}
+
+export default ContactMe
---
+++ src/Components/VideoSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoSection = () => {
+    return (
+        <div className='video-section' id='video-section'>
+            <h1>Video Section</h1>
+        </div>
+    )
+}
+
+export default VideoSection
