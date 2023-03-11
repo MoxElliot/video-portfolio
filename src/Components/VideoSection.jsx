d45a99235c7b4589f55838a18cf31e9e8dc6a592
Elliot Erickson <elliot_erickson@protonmail.com>
2023-03-11 10:58:55 -0800

    Carousel spacing and dark variant

--- src/Components/VideoSection.jsx
+++ src/Components/VideoSection.jsx
@@ -15,22 +15,21 @@ const VideoSection = () => {
     return (
         <div className='video-section' id='video-section'>
             <h1>Video Section</h1>
-            <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel activeIndex={index} onSelect={handleSelect} variant='dark' className='p-5'>
                 <Carousel.Item>
                     <Stack
                         direction="horizontal"
-                        className="h-100 justify-content-center align-items-center"
+                        className="h-100 d-flex justify-content-around"
                         gap={3}>
-                        <Card>
+                        <Card className=''>
                             <Image
-                                 
+                                className='d-block w-100'
                                 src={VideoPlaceholder}
-                                roundedCircle
                                 alt='First Video First Set'
                             />
                             <h5>Video 1/1</h5>
                         </Card>
-                        <Card>
+                        <Card className=''>
                             <img
                                 className='d-block w-100'
                                 src={VideoPlaceholder}
@@ -38,7 +37,7 @@ const VideoSection = () => {
                             />
                             <h5>Video 2/1</h5>
                         </Card>
-                        <Card>
+                        <Card className=''>
                             <img
                                 className='d-block w-100'
                                 src={VideoPlaceholder}
@@ -47,9 +46,77 @@ const VideoSection = () => {
                             <h5>Video 3/1</h5>
                         </Card>
                     </Stack>
-                    <Carousel.Caption>
+                    {/* <Carousel.Caption>
                         <h3>First Set of Slides</h3>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Stack
+                        direction="horizontal"
+                        className="h-100 d-flex justify-content-around"
+                        gap={3}>
+                        <Card>
+                            <Image
+                                className='d-block w-100'
+                                src={VideoPlaceholder}
+                                alt='First Video First Set'
+                            />
+                            <h5>Video 1/2</h5>
+                        </Card>
+                        <Card>
+                            <img
+                                className='d-block w-100'
+                                src={VideoPlaceholder}
+                                alt='Second Video First Set'
+                            />
+                            <h5>Video 2/2</h5>
+                        </Card>
+                        <Card>
+                            <img
+                                className='d-block w-100'
+                                src={VideoPlaceholder}
+                                alt='Third Video First Set'
+                            />
+                            <h5>Video 3/2</h5>
+                        </Card>
+                    </Stack>
+                    {/* <Carousel.Caption>
+                        <h3>First Set of Slides</h3>
+                    </Carousel.Caption> */}
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Stack
+                        direction="horizontal"
+                        className="h-100 d-flex justify-content-around"
+                        gap={3}>
+                        <Card>
+                            <Image
+                                className='d-block w-100'
+                                src={VideoPlaceholder}
+                                alt='First Video First Set'
+                            />
+                            <h5>Video 1/3</h5>
+                        </Card>
+                        <Card>
+                            <img
+                                className='d-block w-100'
+                                src={VideoPlaceholder}
+                                alt='Second Video First Set'
+                            />
+                            <h5>Video 2/3</h5>
+                        </Card>
+                        <Card>
+                            <img
+                                className='d-block w-100'
+                                src={VideoPlaceholder}
+                                alt='Third Video First Set'
+                            />
+                            <h5>Video 3/3</h5>
+                        </Card>
+                    </Stack>
+                    {/* <Carousel.Caption>
+                        <h3>First Set of Slides</h3>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
             </Carousel>
         </div>
