ef1231087749fe0c09f4fe0113107218dc72b01e
Elliot Erickson <elliot_erickson@protonmail.com>
2023-03-17 11:13:50 -0700

    converted all videos to mp4

--- src/Components/VideoSection.jsx
+++ src/Components/VideoSection.jsx
@@ -25,27 +25,26 @@ const VideoSection = () => {
     };
 
     let videoObj= [
-        {src: 'JPicVid/vid1.MOV', title:"Video 1"},
-        {src: 'JPicVid/vid2.MOV', title:"Video 2"},
-        {src: 'JPicVid/vid3.MOV', title:"Video 3"},
-        {src: 'JPicVid/vid4.MOV', title:"Video 4"},
-        {src: 'JPicVid/vid5.MP4', title:"Video 5"},
-        {src: 'JPicVid/vid6.MP4', title:"Video 6"},
-        {src: 'JPicVid/vid7.MOV', title:"Video 7"},
-        {src: 'JPicVid/vid8.MOV', title:"Video 8"},
-        {src: 'JPicVid/vid9.MOV', title:"Video 9"},
-        {src: 'JPicVid/vid10.MOV', title:"Video 10"},
-        {src: 'JPicVid/vid11.MOV', title:"Video 11"},
-        {src: 'JPicVid/vid12.MP4', title:"Video 12"},
+        {src: 'JPicVid/vid1.MP4', title:'Video 1', type:'video/mp4'},
+        {src: 'JPicVid/vid2.MP4', title:'Video 2', type:'video/mp4'},
+        {src: 'JPicVid/vid3.MP4', title:'Video 3', type:'video/mp4'},
+        {src: 'JPicVid/vid4.MP4', title:'Video 4', type:'video/mp4'},
+        {src: 'JPicVid/vid5.MP4', title:'Video 5', type:'video/mp4'},
+        {src: 'JPicVid/vid6.MP4', title:'Video 6', type:'video/mp4'},
+        {src: 'JPicVid/vid7.MP4', title:'Video 7', type:'video/mp4'},
+        {src: 'JPicVid/vid8.MP4', title:'Video 8', type:'video/mp4'},
+        {src: 'JPicVid/vid9.MP4', title:'Video 9', type:'video/mp4'},
+        {src: 'JPicVid/vid10.MP4', title:'Video 10', type:'video/mp4'},
+        {src: 'JPicVid/vid11.MP4', title:'Video 11', type:'video/mp4'},
+        {src: 'JPicVid/vid12.MP4', title:'Video 12', type:'video/mp4'},
     ]
 
     let videos = videoObj.map((video) => {
         return (
                 <div className='d-flex flex-column justify-content-center'>
-                    <img src={video.src} width={300} height={300} className='m-3' />
-                    {/* <img src={video.src} className='m-3' /> */}
-                    {/* <p>{video.title}</p> */}
-                    {/* <img src={JDrive}/> */}
+                    <video className='m-3' controls width={300} height={300}>
+                        <source src={video.src} type={video.type}  />
+                    </video>
                 </div>
                 )
     });
