c04da1ed53732fccf444373576bbf898fb7849fd
Angela Sun <angela.m.sun@gmail.com>
2023-03-17 10:37:13 -0700

    Build mobile first structure shell

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const AboutMe = () => {
     return (
         <div className='about-me' id='about-me'>
-            <h1>About Me</h1>
+            <div className='about-photo'></div>
+            <div className='about-header'>
+                <h1>Elliot's Nephew</h1>
+            </div>
+            <div className='about-stats'></div>
+            <div className='about-summary'>
+                <p>Nowhere in the world will you have seen talent like Elliot’s Nephew. That’s why he has a better website than any other player.</p>
+                
+                <p>He’ll have an even better introduction written.</p>
+
+                <p>And probably a lot more sentences than this. But really, it’ll be pretty short because who comes here to read paragraphs of text?</p>
+            </div>
         </div>
     )
 }
