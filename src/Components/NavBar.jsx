6079d92097466651bf29e4ef5efcbc0ec35be593
Elliot Erickson <elliot_erickson@protonmail.com>
2023-03-19 11:22:10 -0700

    Dropdown navbar for sm screen, regular links for larger

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,38 +2,32 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const NavBar = () => {
     return (
         <>
         {['sm'].map((expand) => (
-            <Navbar key={expand} expand={expand} className='mb-3'>
+            <Navbar key={expand} expand={expand} className='mb-3 '>
                 <Container fluid>
                     <Navbar.Brand>
                         Jacob Doffing
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                    <Navbar.Offcanvas
-                        id={`offcanvasNavbar-expand-${expand}`}
-                        aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                        placement='end'
-                    >
-                    <Offcanvas.Header closeButton>
-                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                        Offcanvas
-                    </Offcanvas.Title>
-                    </Offcanvas.Header>
-                    <Offcanvas.Body>
-                        <Nav className="justify-content-end flex-grow-1 pe-3">
-                            <Nav.Link href="#action1">About Me</Nav.Link>
-                            <Nav.Link href="#action2">Videos</Nav.Link>
-                            <Nav.Link href="#action2">Stats</Nav.Link>
-                            <Nav.Link href="#action2">Contact</Nav.Link>
-                        </Nav>
-                        
-                    </Offcanvas.Body>
-                    </Navbar.Offcanvas>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand} `}>
+                    <NavDropdown className="justify-content-end flex-grow-1 pe-3 ">
+                        <Nav.Link href="#action1">About Me</Nav.Link>
+                        <Nav.Link href="#action2">Videos</Nav.Link>
+                        <Nav.Link href="#action2">Stats</Nav.Link>
+                        <Nav.Link href="#action2">Contact</Nav.Link>
+                    </NavDropdown>
+                    </Navbar.Toggle>
+                    <Nav className="justify-content-end flex-grow-1 pe-3 d-none d-sm-flex">
+                        <Nav.Link href="#action1">About Me</Nav.Link>
+                        <Nav.Link href="#action2">Videos</Nav.Link>
+                        <Nav.Link href="#action2">Stats</Nav.Link>
+                        <Nav.Link href="#action2">Contact</Nav.Link>
+                    </Nav>
                 </Container>
             </Navbar>
         ))}
