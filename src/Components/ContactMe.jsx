5025679ec3d9954ead5ace23a951fd7e23068cb7
Elliot Erickson <elliot_erickson@protonmail.com>
2023-04-04 16:50:47 -0700

    redid contact me styling

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import {AiOutlineMail, AiOutlineInstagram, AiFillTwitterCircle} from 'react-icons/ai';
-import {FaTiktok} from 'react-icons/fa';
+import {FaYoutube} from 'react-icons/fa';
 
 const ContactMe = () => {
     return (
@@ -11,17 +11,17 @@ const ContactMe = () => {
                 style={{height:'10rem'}}
             />
             <ButtonGroup aria-label="Social Contacts" className='contact-card d-flex flex-row p-2'>
-                <a href='mailto:test@gmail.com' className='w-25 h-25'>
-                    <AiOutlineMail className='w-75 h-100' style={{color:'blue'}}/>
+                <a href='mailto:jacobdoffing@gmail.com' className='w-25 h-25'>
+                    <AiOutlineMail className='social-link w-75 h-100'/>
                 </a>
-                <a href='#contact-me' className='w-25 h-25'>
-                    <AiOutlineInstagram className='w-75 h-75' style={{color:'blue'}}/>
+                <a href='https://www.instagram.com/jacobdoffing/' className='w-25 h-25'>
+                    <AiOutlineInstagram className='social-link w-75 h-75'/>
                 </a>
-                <a href='#contact-me' className='w-25 h-25'>
-                    <AiFillTwitterCircle className='w-75 h-75' style={{color:'blue'}}/>
+                <a href='https://twitter.com/jcd1724' className='w-25 h-25'>
+                    <AiFillTwitterCircle className='social-link w-75 h-75'/>
                 </a>
-                <a href='#contact-me' className='w-25 h-25'>
-                    <FaTiktok className='w-75 h-75' style={{color:'blue'}}/>
+                <a href='https://www.youtube.com/watch?v=QwJQGzvBa74' className='w-25 h-25'>
+                    <FaYoutube className='social-link w-75 h-75' />
                 </a>
             </ButtonGroup>
         </div>
